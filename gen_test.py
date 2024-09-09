edb9bc469bceb888312cdc54e303d207ab07530d
kingofheven <343131996@qq.com>
2024-09-09 21:09:28 +0800

    init file

--- gen_test.py
+++ gen_test.py
@@ -12,6 +12,105 @@ import asyncio
 import torch
 import fitlog
 
+
+
+import nltk
+from nltk.translate.meteor_score import meteor_score
+from nltk.tokenize import word_tokenize
+
+nltk.download('wordnet')
+nltk.download('punkt')
+
+def calculate_meteor_score(reference_list, hypothesis_list):
+
+    if len(reference_list) != len(hypothesis_list):
+        raise ValueError("ERROR")
+
+    scores = []
+    for reference, hypothesis in zip(reference_list, hypothesis_list):
+        reference_tokens = word_tokenize(reference)
+        hypothesis_tokens = word_tokenize(hypothesis)
+        score = meteor_score([reference_tokens], hypothesis_tokens)
+        scores.append(score)
+
+    average_score = sum(scores) / len(scores)
+    return average_score
+
+
+
+import sacrebleu
+
+
+def calculate_chrf(reference_list, hypothesis_list):
+
+    if len(reference_list) != len(hypothesis_list):
+        raise ValueError("ERROR")
+
+
+    references = [reference_list]
+
+
+    score = sacrebleu.corpus_chrf(hypothesis_list, references)
+
+    return score.score
+
+
+
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+import nltk
+
+
+
+
+
+def get_synonyms(word):
+
+    synonyms = set()
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            synonyms.add(lemma.name())
+    return list(synonyms)
+
+
+def modified_bleu(reference, hypothesis):
+
+    reference_tokens = word_tokenize(reference)
+    hypothesis_tokens = word_tokenize(hypothesis)
+
+
+    smooth_func = SmoothingFunction().method1
+    base_bleu_score = sentence_bleu([reference_tokens], hypothesis_tokens, smoothing_function=smooth_func)
+
+
+    synonym_matches = 0
+    for token in hypothesis_tokens:
+        synonyms = get_synonyms(token)
+        if any(syn in reference_tokens for syn in synonyms):
+            synonym_matches += 1
+
+    synonym_score = synonym_matches / len(hypothesis_tokens) if hypothesis_tokens else 0
+
+
+    final_score = (base_bleu_score + synonym_score) / 2
+    return final_score
+
+
+def calculate_crystal_bleu_scores(reference_list, hypothesis_list):
+
+    if len(reference_list) != len(hypothesis_list):
+        raise ValueError("ERROR")
+
+    scores = []
+    for reference, hypothesis in zip(reference_list, hypothesis_list):
+        score = modified_bleu(reference, hypothesis)
+        scores.append(score)
+
+    average_score = sum(scores) / len(scores)
+    return average_score
+
+
 def renorm(text):
     text = text.split("\n")[0]
     text = re.sub("[\d]+\#\) ",";", text)
@@ -19,28 +118,10 @@ def renorm(text):
 
 import argparse
 
-def dwiki_bleu(file_name):
-    from tqdm import tqdm
-    from nltk.translate.bleu_score import corpus_bleu
-    from nltk.tokenize import word_tokenize
-    import re, string
-    ref_list = []
-    hyp_list = []
-    with open(file_name) as f:
-        data = json.load(f)
-    punctuation = '[%s]+' % re.escape(string.punctuation)
-    for line in tqdm(data):
-        ref = line['tgt'] if 'tgt' in line else line['answers'][0]
-        hyp = line['generated'].split("<|endoftext|>")[0].strip()
-
-        ref = re.sub(punctuation, '', ref)
-        hyp = re.sub(punctuation, '', hyp)
-        ref_list.append([word_tokenize(ref)])
-        hyp_list.append(word_tokenize(hyp))
-    print('bleu score:{}'.format(corpus_bleu(ref_list, hyp_list)))
 
 
-def wikiauto_bleu(file_name):
+
+def test_bleu(file_name):
     from tqdm import tqdm
     from nltk.translate.bleu_score import corpus_bleu
     from nltk.tokenize import word_tokenize
@@ -181,12 +262,26 @@ def wikiauto_bleu(file_name):
                 rouge_l_score = calculate_rouge_l_f1([" ".join(i[0]) for i in ref_list], [" ".join(i) for i in hyp_list])
                 print(f"Average ROUGE-L F1 Score: {float(rouge_l_score):.4f}")
             
+                average_score = calculate_meteor_score([" ".join(i[0]) for i in ref_list], [" ".join(i) for i in hyp_list])
+                print(f"Average METEOR Score: {float(average_score):.4f}")
+                
+                chrf_score = calculate_chrf([" ".join(i[0]) for i in ref_list], [" ".join(i) for i in hyp_list])
+                print(f"Average chrF Score: {float(chrf_score):.4f}")
+                
+                
+            if args.dataset in ['b2f_small','b2f_medium']:
+                crystal_bleu_score = calculate_crystal_bleu_scores([" ".join(i[0]) for i in ref_list], [" ".join(i) for i in hyp_list])
+                print(f"Average CrystalBLEU Score: {float(crystal_bleu_score):.4f}")    
+                
             if args.dataset in ['conala']:
                 from codebleu import calc_codebleu
 
                 codebleu = calc_codebleu([" ".join(i[0]) for i in ref_list], [" ".join(i) for i in hyp_list], lang="python", weights=(0.25, 0.25, 0.25, 0.25))
                 print('codebleu score:{}'.format(codebleu))
                 print('codebleu score:{}'.format(codebleu['codebleu']))
+                
+                crystal_bleu_score = calculate_crystal_bleu_scores([" ".join(i[0]) for i in ref_list], [" ".join(i) for i in hyp_list])
+                print(f"Average CrystalBLEU Score: {float(crystal_bleu_score):.4f}")
         else:
             bleu = round(corpus_bleu(ref_list, hyp_list), 4)
             fitlog.add_best_metric({args.split: {'bleu': bleu}})
@@ -203,191 +298,20 @@ def wikiauto_bleu(file_name):
 
 
 
-def kp20k_f1(file_name, split):
-    from evaluate_prediction import main, ARGS
-    import re
-    from tqdm import tqdm
-    with open(file_name, 'r') as f:
-        data = json.load(f)
-    src_list = []
-    trg_list = []
-    pred_list = []
-    opt = ARGS()
-    for i in tqdm(data):
-
-        try:
-            d = i['document'] if "document" in i else i['question']
-            src_list.append(d)
-            if split == 'abstract':
-                if "abstractive_keyphrases" in i:
-                    i['abstractive_keyphrases'] = eval(i['abstractive_keyphrases'])
-                    trg_list.append(";".join(i['abstractive_keyphrases']))
-
-                else:
-                    i['abstractive_keyphrases'] = eval(i['answers'][0])
-                    trg_list.append(";".join(i['abstractive_keyphrases']))
-            else:
-                if "extractive_keyphrases" in i:
-                    i['extractive_keyphrases'] = eval(i['extractive_keyphrases'])
-                    trg_list.append(";".join(i['extractive_keyphrases']))
-
-                else:
-                    i['extractive_keyphrases'] = eval(i['answers'][0])
-                    trg_list.append(";".join(i['extractive_keyphrases']))
 
 
-            pred = '"' + i['generated']
-            reg = re.compile(r'"(.*?)"')
-            pred = re.findall(reg, pred)
-            if len(pred) == 0:
-                print(i['generated'])
-            pred_list.append(";".join(pred))
-        except Exception as e:
-            print(e)
-
-    result_dict = main(opt,src_list,trg_list,pred_list)
-    for k,v in result_dict.items():
-        if '@M' in k and 'macro' in k:
-            print('{} : {}'.format(k,v))
-
-def add_mtop_acc(file_name,id_list=None):
-    correct_list = []
-    with open(file_name) as f:
-        line_list = []
-        data = json.load(f)
-
-        for line in data:
-            if id_list is not None and line['id'] not in id_list:
-                continue
-            lf = line['logical_form'] if 'logical_form' in line else line['answers'][0]
-            line_list.append((line['generated'].split("<|endoftext|>")[0].strip(),lf))
-    pred,gold = list(zip(*line_list))
-    res_list = eval_many_mtop(pred,gold)
-
-    for entry,acc in zip(data,res_list):
-        entry['acc'] =acc
 
 
-    res_list_int = list(map(int,res_list))
-
-    acc_result = sum(res_list_int)/len(res_list_int)
-    print('file_name:{}'.format(file_name))
-    print('prediction acc:{}'.format(acc_result))
-    fitlog.add_best_metric({args.split: {'acc': acc_result}})
-    return data
 
 
-def add_spider_acc(file_name,id_list=None):
-    from datasets import load_from_disk
-    import spider.evaluation
-    from tqdm import tqdm
-    with open(file_name, 'r') as f:
-        input_data = json.load(f)
-    ds = load_from_disk("/nvme/xnli/lk_code/exps/rtv_icl/data/spider")
-
-    db_dict = {}
-    for e in ds['validation']:
-        db_dict[e['query']] = e['db_id']
-
-    pred_list = []
-    gold_list = []
-    num = 0
-    for e in tqdm(input_data):
-        p = e['generated'].split("<|endoftext|>")[0].strip()
-        g = e['query'] if 'query' in e else e['answers'][0]
-        db_id = db_dict[g]
-
-        if p == g:
-            num += 1
-        g = [g, db_id]
-        if len(p) == 0:
-            p = "a"
-        p = [p, db_id]
-        pred_list.append(p)
-        gold_list.append(g)
-    ret = spider.evaluation.evaluate_in_memory(pred_list, gold_list)
-    fitlog.add_best_metric({args.split: {'acc': ret['total_scores']['all']['exact']}})
-
-def add_break_acc(path, id_list=None):
-    with BufferedJsonReader(path) as f:
-        df = pd.DataFrame(f.read())
-    data = df.to_dict("records")
-    question_field = "question" if "question" in data[0] else 'question_text'
-    zipped_data = []
-    for entry in data:
-        if id_list is not None and entry['id'] in id_list:
-            continue
-        generated = renorm(entry['generated'].split("\n")[0].split("<|endoftext|>")[0]).strip()
-        decomposition = entry['decomposition'] if "decomposition" in entry else entry['answers'][0]
-
-        zipped_data.append([entry[question_field], generated, decomposition])
-
-    questions, pred, gold = list(zip(*zipped_data))
-    acc_results = eval_many(questions, pred, gold)
-    acc_results_int = list(map(int,acc_results))
-    acc_result = sum(acc_results_int)/len(acc_results_int)
-    print('result:')
-    print(acc_result)
-    fitlog.add_best_metric({args.split: {'acc': acc_result}})
-
-    for entry, acc in zip(data, acc_results):
-        entry['acc'] = acc
-    return data
 
 
-def add_smcalflow_acc(file_name,id_list=None):
-    correct_list = []
-    with open(file_name) as f:
-        data = json.load(f)
-        line_list = []
-        for line in data:
-            if id_list is not None and line['id'] not in id_list:
-                continue
-            lf = line['lispress'] if 'lispress' in line else line['answers'][0]
-            line_list.append((line['generated'].split("<|endoftext|>")[0].strip(),lf))
-    pred,gold = list(zip(*line_list))
-    res_list = eval_many_smcalflow(pred,gold)
 
-    acc_results_int = list(map(int,res_list))
-    acc_result = sum(acc_results_int)/len(acc_results_int)
-    print('result:')
-    print(acc_result)
-    fitlog.add_best_metric({args.split: {'acc': acc_result}})
 
-    for entry,acc in zip(data,res_list):
-        entry['acc'] =acc
-    return data
 
 from qdecomp_with_dependency_graphs.scripts.eval.evaluate_predictions import evaluate
 from qdecomp_with_dependency_graphs.scripts.eval.evaluate_predictions import format_qdmr
-def eval_break_test(file_name,id_list=None):
-    with BufferedJsonReader(file_name) as f:
-        df = pd.DataFrame(f.read())
-    data = df.to_dict("records")
-    question_field = "question" if "question" in data[0] else 'question_text'
-    zipped_data = []
-    for entry in data:
-        if id_list is not None and entry['id'] in id_list:
-            continue
-        generated = renorm(entry['generated'].split("\n")[0].split("<|endoftext|>")[0]).strip()
-        decomposition = entry['decomposition'] if "decomposition" in entry else entry['answers'][0]
 
-        zipped_data.append([entry[question_field], generated, decomposition, entry['question_id']])
-
-    questions, predictions, golds, question_ids = list(zip(*zipped_data))
-
-    predictions = [format_qdmr(pred.replace("  "," ")) for pred in predictions]
-    golds = [format_qdmr(gold) for gold in golds]
-
-    res = evaluate(question_ids=question_ids,
-                   questions=questions,
-                   golds=golds,
-                   decompositions=predictions,
-                   metadata=None,
-                   output_path_base=None,
-                   num_processes=8)
-
-    print(res)
 
 
 
@@ -423,7 +347,7 @@ if __name__ == '__main__':
     
 
     if args.dataset in ['tlc','tlc_slice1','csn_go','csn_ruby','csn_js','csn_php','b2f_medium','b2f_small','conala','csn_java_slice1','csn_java','csn_python']:
-        wikiauto_bleu(tmp_fp)
+        test_bleu(tmp_fp)
 
     else:
         raise NotImplementedError
